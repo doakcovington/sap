7b5d6c25acbbd6d1e4e7ee48e4aba200e8a75617
doakcovington <doakcovington@protonmail.com>
2021-01-01 15:08:00 -0800

    Built out display_board method

--- Game.py
+++ Game.py
@@ -3,4 +3,12 @@ class Game:
     def __init__(self,player1,player2):
         self.player1 = player1
         self.player2 = player2
-    
+        self.board = [[""] * 8] * 8
+
+    def display_board(self):
+        for row in self.board:
+            print(row)
+
+
+g1 = Game('Doak', 'Heidi')
+g1.display_board()
