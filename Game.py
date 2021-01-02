3cf90bafa44edded21e984d9c397cb1a7cd8d64d
doakcovington <doakcovington@protonmail.com>
2021-01-02 15:55:09 -0800

    Built out fill_board method

--- Game.py
+++ Game.py
@@ -3,7 +3,13 @@ class Game:
     def __init__(self,player1,player2):
         self.player1 = player1
         self.player2 = player2
-        self.board = [[""] * 8] * 8
+        self.board = [[' '] * 8] * 4
+
+    def fill_board(self):
+        self.board.insert(0, ['X'] * 8)
+        self.board.insert(1, ['X'] * 8)
+        self.board.append(['X'] * 8)
+        self.board.append(['X'] * 8)
 
     def display_board(self):
         for row in self.board:
@@ -11,4 +17,6 @@ class Game:
 
 
 g1 = Game('Doak', 'Heidi')
+g1.fill_board()
 g1.display_board()
+
